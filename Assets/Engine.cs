fe57d98a9baf6363d012b649ca1b3cfba165661c
Berry Hermans <berry.hermans@gamesolutionslab.com>
2018-12-14 14:19:49 +0100

    One small step for DieselSkies. One giant take-off for Berry! Airplane moves forward with given speed

---
+++ Assets/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour {
+
+	public Transform BodyTrans;
+	[Range(0,100)]
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		BodyTrans.Translate(Vector3.forward * Time.deltaTime * Speed);		
+	}
+}
