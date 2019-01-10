38cb2e471a4cbdac99827dd678a1ec9b706fb251
Berry Hermans <berry.hermans@gamesolutionslab.com>
2019-01-10 13:32:37 +0100

    Added statemachine to Wings

--- Assets/DieselSkies/scripts/Wings.cs
+++ Assets/DieselSkies/scripts/Wings.cs
@@ -7,26 +7,66 @@ public class Wings : MonoBehaviour
 	public Rigidbody Body;
     [Range(0,360)]
     public float TurnStrength;
-
 	public InputBroadcaster Input;
+
+	private WingState _currentState;
+	private WingState _previousState;
 	private Vector3 _target;
 
 	// Use this for initialization
 	void Start()
 	{
-		_target = Body.transform.forward.normalized;
 		Input.OnVector3Broadcast += OnInputHandler;
 	}
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Quaternion _targetRotation = Quaternion.LookRotation(_target - Body.position);
-        Body.rotation = Quaternion.RotateTowards(Body.rotation, _targetRotation, TurnStrength * Time.deltaTime);
+		HandleState();
     }
 
 	private void OnInputHandler(Vector3 input)
 	{
 		_target = input;
+		if (_currentState != WingState.TURNING) SwitchState(WingState.TURNING);
 	}
+
+	private void SwitchState(WingState newState)
+	{
+		_previousState = _currentState;
+		_currentState = newState;
+	}
+
+	private void HandleState()
+	{
+		switch (_currentState)
+		{
+			case WingState.IDLE:
+				HandleIdleState();
+				break;
+			case WingState.TURNING:
+				HandleTurningState();
+				break;
+			default:
+				Debug.LogError("Can't handle unknown state: " + _currentState.ToString());
+				break;
+		}
+	}
+
+	private void HandleTurningState()
+	{
+		Quaternion _targetRotation = Quaternion.LookRotation(_target - Body.position);
+		Body.rotation = Quaternion.RotateTowards(Body.rotation, _targetRotation, TurnStrength * Time.deltaTime);
+	}
+
+	private void HandleIdleState()
+	{
+
+	}
+}
+
+public enum WingState
+{
+	IDLE,
+	TURNING
 }
