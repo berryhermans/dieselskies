be70a1a8e1aa2a207b8ec304ef0ecdb3b06f19c9
Berry Hermans <berryhermans@gmail.com>
2019-02-27 19:58:32 +0100

    Fixed bullet spawn issue
    Also renamed engine to MoveForward

---
+++ Assets/DieselSkies/scripts/MoveForward.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveForward : MonoBehaviour {
+
+	[Range(0,1000)]
+	public float Speed;
+
+    private Rigidbody _body;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+		_body.velocity = (_body.rotation * Vector3.forward ) * (Speed * Time.deltaTime);
+	}
+}
