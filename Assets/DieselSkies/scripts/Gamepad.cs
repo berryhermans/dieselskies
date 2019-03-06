74c6a25f7974feceec9deeaa3dd5c518e7ab11a1
Berry Hermans <berryhermans@gmail.com>
2019-03-06 18:36:01 +0100

    super basic gamepad test

---
+++ Assets/DieselSkies/scripts/Gamepad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamepad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (string joystick in Input.GetJoystickNames())
+        {
+            Debug.Log(joystick);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            Debug.Log("beepboop");
+        }
+    }
+}
