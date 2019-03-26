07d4894d62770424d2fdd5124fb0fbde5c1dc936
Berry Hermans <berryhermans@gmail.com>
2019-03-26 19:47:52 +0100

    Created base control scheme and made test gamepad

--- Assets/DieselSkies/scripts/Gamepad.cs
+++ Assets/DieselSkies/scripts/Gamepad.cs
@@ -2,28 +2,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class Gamepad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int PlayerId = 0;
+
+    private Player _player;
+
+    private void Awake()
     {
-        foreach (string joystick in Input.GetJoystickNames())
-        {
-            Debug.Log(joystick);
-        }
+        _player = ReInput.players.GetPlayer(PlayerId);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.anyKeyDown)
-        {
-            foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
-            {
-                if (Input.GetKeyDown(kcode))
-                    Debug.Log("KeyCode down: " + kcode);
-            }
-        }
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        Vector2 axis = new Vector2(_player.GetAxis("Move Horizontal"), _player.GetAxis("Move Vertical"));
+        bool SelectFirst = _player.GetButton("Select First Plane");
+        bool SelectSecond = _player.GetButton("Select Second Plane");
+        bool SelectThird = _player.GetButton("Select Third Plane");
+        bool SelectFourth = _player.GetButton("Select Fourth Plane");
+
+        Debug.Log(string.Format("({0} ; {1}) | first: {2} | second: {3} | third: {4} | foruth: {5}", 
+            axis.x.ToString("0.00"), 
+            axis.y.ToString("0.00"), 
+            SelectFirst, 
+            SelectSecond, 
+            SelectThird, 
+            SelectFourth));
     }
 }
