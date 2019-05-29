bdafe9ce4c4ee81518e1cc6b24527b417cd6abad
Berry Hermans <berryhermans@gmail.com>
2019-05-29 21:15:37 +0200

    Can steer plane with controller. Still has 90 degree offset for some reason.

--- Assets/DieselSkies/scripts/Wings.cs
+++ Assets/DieselSkies/scripts/Wings.cs
@@ -4,71 +4,37 @@ using UnityEngine;
 
 public class Wings : MonoBehaviour
 {
-	public Rigidbody Body;
+	//public Rigidbody Body;
     [Range(0,360)]
-    public float TurnStrength;
+    public float YawRotateStrength;
 	public InputBroadcaster Input;
+    
+    private float _targetYawRotation;
 
-	private WingState _currentState;
-	private WingState _previousState;
-    private float _targetRotationDegreesY;
-
-	// Use this for initialization
 	void Start()
 	{
 		Input.Vector2Broadcasted += OnVector2BroadcastedHandler;
-		SetState(WingState.IDLE);
 	}
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-		HandleState();
+        Yaw();
+		//Turn();
     }
 
 	private void OnVector2BroadcastedHandler(Vector2 input)
 	{
-		_targetRotationDegreesY = (Mathf.Atan2(input.y, input.x) / Mathf.PI) * 180f;
-        if (_targetRotationDegreesY < 0) _targetRotationDegreesY += 360;
-        Debug.Log(_targetRotationDegreesY);
-		if (_currentState != WingState.TURNING) SetState(WingState.TURNING);
+        // Set the target yaw by converting the given unit circle input to degrees
+		_targetYawRotation = (Mathf.Atan2(input.y, input.x) / Mathf.PI) * 180f;
+
+        Debug.Log(string.Format("input: {0} | degrees: {1}", input.ToString(), _targetYawRotation));
 	}
 
-	private void SetState(WingState newState)
-	{
-		_previousState = _currentState;
-		_currentState = newState;
-	}
+    private void Yaw()
+    {
+        if (transform.rotation.y == _targetYawRotation) return;
 
-	private void HandleState()
-	{
-		switch (_currentState)
-		{
-			case WingState.IDLE:
-				// The plane is on target, so the wings don't have to do anything.
-				break;
-			case WingState.TURNING:
-				HandleTurningState();
-				break;
-			default:
-				Debug.LogError("Can't handle unknown state: " + _currentState.ToString());
-				break;
-		}
-	}
-
-	private void HandleTurningState()
-	{
-        Vector3 bla = new Vector3(Body.rotation.x, _targetRotationDegreesY, Body.rotation.z);
-		Quaternion targetRotation = Quaternion.LookRotation(bla, Vector3.up);
-		Body.rotation = Quaternion.RotateTowards(Body.rotation, targetRotation, TurnStrength * Time.deltaTime);
-
-		// Are we on target yet?
-		if (Body.rotation == targetRotation) SetState(WingState.IDLE);
-	}
-}
-
-public enum WingState
-{
-	IDLE,
-	TURNING
+        Quaternion targetRotation = Quaternion.Euler(transform.rotation.x, _targetYawRotation, transform.rotation.z);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, YawRotateStrength * Time.deltaTime);
+    }
 }
