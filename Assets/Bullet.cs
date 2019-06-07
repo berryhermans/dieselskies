e4be81cf4f33c53f58def7b9b6b7133afd5ec0ec
Berry Hermans <berryhermans@gmail.com>
2019-06-07 21:13:49 +0200

    bullet script wip

---
+++ Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+
+        if (true)
+        {
+            // if hitting a hull, do damage and destroy self.
+        }
+    }
+}
