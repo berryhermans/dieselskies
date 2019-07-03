0c397ac045a9a72caebf758cf4785fa11c3a23f7
Berry Hermans <berryhermans@gmail.com>
2019-07-03 21:23:34 +0200

    Fixed plane detection colliders

--- Assets/DieselSkies/Scripts/Bullet.cs
+++ Assets/DieselSkies/Scripts/Bullet.cs
@@ -6,8 +6,6 @@ public class Bullet : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
-
         if (true)
         {
             // if hitting a hull, do damage and destroy self.
