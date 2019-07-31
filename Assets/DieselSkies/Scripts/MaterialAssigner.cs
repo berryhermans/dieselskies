1719047f4ac202eafc14456e97fd86b02c013946
Berry Hermans <berryhermans@gmail.com>
2019-07-31 21:06:36 +0200

    made material assigner but doesn't seem to actually change the material even though all values seem correct

---
+++ Assets/DieselSkies/Scripts/MaterialAssigner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialAssigner : MonoBehaviour
+{
+    public List<MaterialAssignmentModel> Objects;
+
+    public void AssignAllMaterials()
+    {
+        foreach (MaterialAssignmentModel model in Objects)
+        {
+            Debug.Log(string.Format("changing {0}'s {1} to {2}", model.Mesh.name, model.Mesh.materials[model.MaterialIndex].name, model.NewMaterial.name));
+            model.Mesh.materials[model.MaterialIndex] = model.NewMaterial;
+            Debug.Log(string.Format("{0}'s new colour is {1}", model.Mesh.name, model.Mesh.materials[model.MaterialIndex].name));
+        }
+    }
+
+    private void Start()
+    {
+        AssignAllMaterials();
+    }
+}
+
+[Serializable]
+public class MaterialAssignmentModel
+{
+    public MeshRenderer Mesh;
+    public int MaterialIndex;
+    public Material NewMaterial;
+}
