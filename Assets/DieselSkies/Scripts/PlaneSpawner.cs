d3e7378896ae19c56d1d611869b97d6023eb3e96
Berry Hermans <berryhermans@gmail.com>
2019-11-13 19:18:58 +0100

    visible play area

--- Assets/DieselSkies/Scripts/PlaneSpawner.cs
+++ Assets/DieselSkies/Scripts/PlaneSpawner.cs
@@ -6,4 +6,16 @@ public class PlaneSpawner : MonoBehaviour
 {
     public int PlayerId;
 
+    public void Spawn()
+    {
+        // generate random 2d vector
+
+        // get bounding box
+
+        // figure out how long a line with the random vector would be from the bounding box' center to it's edge
+
+        // add some predetermined distance
+        
+        // spawn there
+    }
 }
