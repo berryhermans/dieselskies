6d6c3bdefa684674c70c84cf9c6b671462dc8ca0
Berry Hermans <berryhermans@gmail.com>
2019-12-01 18:41:12 +0100

    PlaneSpawner wip
    determine random point on spawn edge

--- Assets/DieselSkies/Scripts/PlaneSpawner.cs
+++ Assets/DieselSkies/Scripts/PlaneSpawner.cs
@@ -1,21 +1,55 @@
-﻿using System.Collections;
+﻿using JuiceBox;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaneSpawner : MonoBehaviour
 {
     public int PlayerId;
+    public float SpawnOffset;
+    public SpawnEdge Edge;
+    public MeshCollider PlayArea;
 
     public void Spawn()
     {
-        // generate random 2d vector
+        Bounds bounds = PlayArea.bounds;
+        Vector3 min;
+        Vector3 max;
 
-        // get bounding box
+        switch (Edge)
+        {
+            case SpawnEdge.TOP:
+                min = new Vector3(bounds.min.x, bounds.min.y, bounds.max.z);
+                max = bounds.max;
+                break;
 
-        // figure out how long a line with the random vector would be from the bounding box' center to it's edge
+            case SpawnEdge.RIGHT:
+                min = bounds.max;
+                max = new Vector3(bounds.max.x, bounds.min.y, bounds.min.z);
+                break;
 
-        // add some predetermined distance
-        
-        // spawn there
+            case SpawnEdge.BOTTOM:
+                min = new Vector3(bounds.max.x, bounds.min.y, bounds.min.z);
+                max = bounds.min;
+                break;
+
+            case SpawnEdge.LEFT:
+                min = bounds.min;
+                max = new Vector3(bounds.min.x, bounds.min.y, bounds.max.z);
+                break;
+
+            default:
+                Debug.LogErrorFormat("Unknown SpawnEdge '{0}'", Edge);
+                return;
+        }
+        Vector3 RandomPointOnEdge = VectorUtility.RandomRange(min, max);
+    }
+
+    public enum SpawnEdge
+    {
+        TOP,
+        RIGHT,
+        BOTTOM,
+        LEFT
     }
 }
